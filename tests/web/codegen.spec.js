de8dbd6927c0c131faec445a9d4db2c48fa274ad
Amro Moussa <AmrMoussa24>
2025-11-13 16:44:28 +0200

    enhance Frame

--- tests/web/codegen.spec.js
+++ tests/web/codegen.spec.js
@@ -1,4 +1,14 @@
 import { test, expect } from '@playwright/test';
-
+// npx playwright codegen -o ./tests/web/codegen.spec.js 
 test('test', async ({ page }) => {
+  await page.goto('http://the-internet.herokuapp.com/nested_frames');
+  await page.locator('frame[name="frame-top"]').contentFrame().locator('frame[name="frame-middle"]').contentFrame().locator('body').click();
+  await expect(page.locator('frame[name="frame-top"]').contentFrame().locator('frame[name="frame-middle"]').contentFrame().locator('#content')).toContainText('MIDDLE');
+  await expect(page.locator('frame[name="frame-bottom"]').contentFrame().locator('body')).toContainText('BOTTOM');
+  await page.locator('frame[name="frame-top"]').contentFrame().locator('frame[name="frame-right"]').contentFrame().getByText('RIGHT').click();
+  await page.locator('frame[name="frame-top"]').contentFrame().locator('frame[name="frame-middle"]').contentFrame().locator('body').click();
+  await page.locator('frame[name="frame-top"]').contentFrame().locator('frame[name="frame-middle"]').contentFrame().locator('body').click();
+  await page.locator('frame[name="frame-top"]').contentFrame().locator('frame[name="frame-left"]').contentFrame().getByText('LEFT').click();
+  await page.locator('frame[name="frame-top"]').contentFrame().locator('frame[name="frame-left"]').contentFrame().getByText('LEFT').click();
+  await page.locator('frame[name="frame-top"]').contentFrame().locator('frame[name="frame-middle"]').contentFrame().locator('body').click();
 });
